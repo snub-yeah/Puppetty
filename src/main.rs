089076a7b6e522e2aaaf6f9635ca4d313757b04f
snub-yeah <snublol@proton.me>
2026-07-18 12:55:06 -0500

    Test button lol

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use bevy::feathers::controls::FeathersButton;
 use bevy::feathers::dark_theme::create_dark_theme;
-use bevy::feathers::theme::UiTheme;
+use bevy::feathers::theme::{ThemeBackgroundColor, UiTheme};
 use bevy::feathers::*;
+use bevy::input_focus::tab_navigation::TabGroup;
 use bevy::prelude::*;
+use bevy::ui_widgets::Activate;
 
 fn main() {
     App::new()
@@ -12,5 +15,32 @@ fn main() {
 }
 
 fn scene() -> impl SceneList {
-    bsn_list![Camera2d]
+    bsn_list![Camera2d, config_window()]
+}
+
+fn config_window() -> impl Scene {
+    bsn![Node {
+            width: percent(100),
+            height: percent(100),
+            align_items: AlignItems::Start,
+            justify_content: JustifyContent::Start,
+            display: Display::Flex,
+            flex_direction: FlexDirection::Row,
+            column_gap: px(8),
+        }
+        TabGroup
+        ThemeBackgroundColor(tokens::WINDOW_BG)
+        Children[(
+            @FeathersButton{
+                @caption: bsn!{ Text("Test")}
+            }
+            Node{
+                flex_grow: 1.0,
+                margin: UiRect::horizontal(percent(2.0))
+            }
+            on(|_activate: On<Activate>| {
+                info!("Button activated");
+            })
+    )    ]
+        ]
 }
